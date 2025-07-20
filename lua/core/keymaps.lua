3202144f5a1a98f511f53ab50005e9507aa6711c
Moshe Avni <moshe.avni@flexera.com>
2025-07-20 21:34:32 +0300

    move stuff around

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,9 @@
 local map = vim.keymap.set
 
+-- Search
 map('n', '<CR>', '<Esc>:nohlsearch<CR><CR>', { remap = false, silent = true, desc = 'Clear search highlighting' })
+
+-- Navigation
 map({ 'n', 'v' }, '0', '^', { remap = false, desc = 'Go to the first non-blank character' })
 -- Move view left or right
 map('n', 'L', '5zl', { remap = false, desc = 'Move view to the right' })
@@ -8,11 +11,16 @@ map('v', 'L', '$', { remap = false, desc = 'Move view to the right' })
 map('n', 'H', '5zh', { remap = false, desc = 'Move view to the left' })
 map('v', 'H', '0', { remap = false, desc = 'Move view to the left' })
 
+-- Scrolling centralized
+map('n', '<C-u>', '<C-u>zz', { remap = false, desc = 'Scroll half page up and center' })
+map('n', '<C-d>', '<C-d>zz', { remap = false, desc = 'Scroll half page down and center' })
+
+-- Visual Mode
 -- indent/unindent visual mode selection with tab/shift+tab
 map('v', '<tab>', '>gv', { desc = 'Indent selected text' })
 map('v', '<s-tab>', '<gv', { desc = 'Unindent selected text' })
 
--- Windows mappings
+-- Windows
 map('n', '<c-w>v', ':vnew<cr>', { remap = false, silent = true, desc = 'New buffer vertically split' })
 map('n', '<c-w>s', ':new<cr>', { remap = false, silent = true, desc = 'New buffer horizontally split' })
 map('n', '<c-w>e', ':enew<cr>', { remap = false, silent = true, desc = 'New empty buffer' })
@@ -21,51 +29,39 @@ map('n', '<Down>', '<C-w>j', { remap = true, desc = 'Go to Lower Window' })
 map('n', '<Up>', '<C-w>k', { remap = true, desc = 'Go to Upper Window' })
 map('n', '<Right>', '<C-w>l', { remap = true, desc = 'Go to Right Window' })
 
-map('n', ']q', ':cnext<cr>zz', { remap = false, silent = true })
-map('n', '[q', ':cprev<cr>zz', { remap = false, silent = true })
+-- Quickfix
+map('n', ']q', ':cnext<cr>zz', { remap = false, silent = true, desc = 'Next quickfix item' })
+map('n', '[q', ':cprev<cr>zz', { remap = false, silent = true, desc = 'Previous quickfix item' })
 
 -- Terminal
 map('t', '<Esc>', [[<C-\><C-n>]], { remap = false, desc = 'Exit terminal mode' })
 
+-- File Operations
 -- Copy file path to clipboard
 map(
   'n',
   '<leader>cfp',
   [[:let @+ = expand('%')<cr>:echo "Copied relative file path " . expand('%')<cr>]],
-  { remap = false, silent = true }
+  { remap = false, silent = true, desc = 'Copy relative file path' }
 )
 map(
   'n',
   '<leader>cfa',
   [[:let @+ = expand('%:p')<cr>:echo "Copied full file path " . expand('%:p')<cr>]],
-  { remap = false, silent = true }
+  { remap = false, silent = true, desc = 'Copy absolute file path' }
 )
 map(
   'n',
   '<leader>cfd',
   [[:let @+ = expand('%:p:h')<cr>:echo "Copied file directory path " . expand('%:p:h')<cr>]],
-  { remap = false, silent = true }
+  { remap = false, silent = true, desc = 'Copy file directory path' }
 )
 map(
   'n',
   '<leader>cfn',
   [[:let @+ = expand('%:t')<cr>:echo "Copied file directory path " . expand('%:t')<cr>]],
-  { remap = false, silent = true }
+  { remap = false, silent = true, desc = 'Copy file name' }
 )
-
--- Copy and paste to/from system clipboard
-map('v', 'cp', '"+y', { desc = 'Copy to system clipboard' })
-map('n', 'cP', '"+yy', { desc = 'Copy line to system clipboard' })
-map('n', 'cp', '"+y', { desc = 'Copy to system clipboard' })
-map('n', 'cv', '"+p', { desc = 'Paste from system clipboard' })
-map('n', '<C-c>', 'ciw', { desc = 'Change inner word' })
-map('n', '<C-c>', 'ciw')
--- Copy entire file to clipboard
-map('n', 'Y', ':%y+<cr>', { remap = false, silent = true, desc = 'Copy buffer content to clipboard' })
-
--- Scrolling centralized
-map('n', '<C-u>', '<C-u>zz', { remap = false, desc = 'Scroll half page up and center' })
-map('n', '<C-d>', '<C-d>zz', { remap = false, desc = 'Scroll half page down and center' })
 
 -- Change working directory based on open file
 map(
@@ -75,6 +71,20 @@ map(
   { remap = false, silent = true, desc = 'Change directory to current file' }
 )
 
+-- Copy and Paste
+-- Copy and paste to/from system clipboard
+map('v', 'cp', '"+y', { desc = 'Copy to system clipboard' })
+map('n', 'cP', '"+yy', { desc = 'Copy line to system clipboard' })
+map('n', 'cp', '"+y', { desc = 'Copy to system clipboard' })
+map('n', 'cv', '"+p', { desc = 'Paste from system clipboard' })
+map('n', '<C-c>', 'ciw', { desc = 'Change inner word' })
+-- Copy entire file to clipboard
+map('n', 'Y', ':%y+<cr>', { remap = false, silent = true, desc = 'Copy buffer content to clipboard' })
+
+-- Application
+map('n', '<leader>qq', ':qall<cr>', { remap = false, silent = true, desc = 'Quit all' })
+
+-- Diff
 vim.api.nvim_create_user_command('DiffWithSaved', function()
   -- Get start buffer
   local start = vim.api.nvim_get_current_buf()
@@ -94,4 +104,5 @@ vim.api.nvim_create_user_command('DiffWithSaved', function()
   end
 end, {})
 
-map('n', '<leader>ds', ':DiffWithSaved<cr>', { remap = false, silent = true })
+map('n', '<leader>ds', ':DiffWithSaved<cr>', { remap = false, silent = true, desc = 'Diff with saved version' })
+
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,65 +1,54 @@
+-- Colorscheme
 vim.cmd [[colorscheme default]]
 
--- general options
-vim.o.completeopt = 'menu,menuone,noselect,popup,fuzzy' -- modern completion menu
-
-vim.o.foldenable = true                                 -- enable fold
-vim.o.foldlevel = 99                                    -- start editing with all folds opened
-vim.o.foldmethod = 'expr'                               -- use tree-sitter for folding method
-vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-vim.o.foldtext = 'substitute(getline(v:foldstart),"\t",repeat(" ",&tabstop),"g")."...".trim(getline(v:foldend))'
-
+-- Display & UI
 vim.o.title = true
 vim.o.cursorcolumn = true
 vim.o.cursorline = true
-vim.opt.shortmess:append { c = true, l = false, q = false, S = false, C = true, I = true }
-vim.o.list = true
-vim.o.shada = [[!,'1000,s10000,h]]
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.linebreak = true
+vim.o.signcolumn = 'yes'
+vim.o.cmdheight = 1
+vim.o.laststatus = 3
+vim.o.showcmd = true
+vim.o.showmatch = true
+vim.o.matchtime = 2 -- How long to show matching bracket
+vim.o.winborder = 'rounded'
+vim.o.wrap = false -- Don't wrap lines
+vim.o.conceallevel = 0 -- Don't hide markup
+vim.o.concealcursor = '' -- Don't hide cursor line markup
+vim.opt.shortmess:append { c = true, l = false, q = false, S = false, C = true, I = true }
+vim.opt.pumblend = 10 -- Popup menu transparency
+vim.opt.winblend = 0 -- Floating window transparency
+
+-- Search
 vim.o.hlsearch = true
 vim.o.incsearch = true
 vim.o.inccommand = 'split'
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.autoread = true
+
+-- Scrolling
 vim.o.scrolloff = 4
 vim.o.sidescrolloff = 8
-vim.o.cmdheight = 1
-vim.o.hidden = true
-vim.o.showmatch = true
+
+-- Split Behavior
 vim.o.splitbelow = true
 vim.o.splitright = true
-vim.o.startofline = false
-vim.o.confirm = true
-vim.o.lazyredraw = false
-vim.o.swapfile = false
-vim.o.wildmenu = true
-vim.opt.wildmode = { 'longest:full', 'full' }
-vim.o.previewheight = 15
-vim.o.laststatus = 3
-vim.o.showcmd = true
-vim.o.mouse = 'a'
-vim.o.undofile = true
-vim.o.undolevels = 10000
-vim.o.textwidth = 80
-vim.opt.cpoptions:append '>'
 vim.o.equalalways = true
-vim.o.history = 10000
-vim.o.signcolumn = 'yes'
-vim.o.updatetime = 300
-vim.opt.wildignore:append { '**/node_modules/**', '.hg', '.git', '.svn', '*.DS_Store', '*.pyc' }
-vim.opt.path:append { '**' }
-vim.opt.formatoptions:append {
-  c = true,
-  j = true,
-  l = true,
-  o = true,
-  r = true,
-  t = true,
-}
--- Indenting
+
+-- Completion
+vim.o.completeopt = 'menu,menuone,noselect,noinsert,popup,fuzzy' -- modern completion menu
+vim.o.pumheight = 10 -- max height of completion menu
+
+-- Folding
+vim.o.foldenable = true -- enable fold
+vim.o.foldlevel = 99 -- start editing with all folds opened
+vim.o.foldmethod = 'expr' -- use tree-sitter for folding method
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldtext = 'substitute(getline(v:foldstart),"\t",repeat(" ",&tabstop),"g")."...".trim(getline(v:foldend))'
+
+-- Indentation
 vim.o.breakindent = true
 vim.o.autoindent = true
 vim.o.copyindent = true
@@ -72,11 +61,9 @@ vim.o.smarttab = true
 vim.o.expandtab = true
 vim.opt.indentkeys:remove '0#'
 vim.opt.indentkeys:remove '<:>'
-vim.o.winborder = 'rounded'
 
-vim.o.pumheight = 10  -- max height of completion menu
-
-vim.o.list = true     -- use special characters to represent things like tabs or trailing spaces
+-- List Characters
+vim.o.list = true -- use special characters to represent things like tabs or trailing spaces
 vim.opt.listchars = { -- NOTE: using `vim.opt` instead of `vim.o` to pass rich object
   tab = '▏ ',
   trail = '·',
@@ -84,4 +71,62 @@ vim.opt.listchars = { -- NOTE: using `vim.opt` instead of `vim.o` to pass rich o
   precedes = '«',
 }
 
+-- File & Buffer Management
+vim.o.autoread = true
+vim.o.autowrite = false -- Don't auto save
+vim.o.hidden = true
+vim.o.backup = false -- Don't create backup files
+vim.o.writebackup = false -- Don't create backup before writing
+vim.o.swapfile = false
+vim.o.undofile = true
+vim.o.undodir = vim.fn.stdpath 'state' .. '/undo' -- Undo directory
+vim.o.undolevels = 10000
+vim.o.shada = [[!,'1000,s10000,h]]
+vim.opt.wildignore:append { '**/node_modules/**', '.hg', '.git', '.svn', '*.DS_Store', '*.pyc' }
+vim.opt.path:append { '**' }
+
+-- Create undo directory if it doesn't exist
+local undodir = vim.fn.stdpath 'state' .. '/undo'
+if vim.fn.isdirectory(undodir) == 0 then
+  vim.fn.mkdir(undodir, 'p')
+end
+
+-- Performance
+vim.o.lazyredraw = true
+vim.opt.synmaxcol = 300
+vim.o.updatetime = 300
+
+-- Command & Wildmenu
+vim.o.wildmenu = true
+vim.opt.wildmode = { 'longest:full', 'full' }
+vim.o.previewheight = 15
+vim.o.history = 10000
+
+-- Text Editing
+vim.o.linebreak = true
+vim.o.startofline = false
+vim.o.confirm = true
+vim.o.textwidth = 80
+vim.opt.cpoptions:append '>'
+vim.opt.formatoptions:append {
+  c = true,
+  j = true,
+  l = true,
+  o = true,
+  r = true,
+  t = true,
+}
+
+-- Input
+vim.o.mouse = 'a'
+
+-- Behavior
+vim.o.errorbells = false -- No error bells
+vim.o.backspace = 'indent,eol,start' -- Better backspace behavior
+vim.o.autochdir = false -- Don't auto change directory
+vim.opt.iskeyword:append '-' -- Treat dash as part of word
+vim.o.selection = 'exclusive' -- Selection behavior
+vim.o.encoding = 'UTF-8' -- Set encoding
+
+-- Diff Options
 vim.opt.diffopt:append 'linematch:60' -- second stage diff to align lines
