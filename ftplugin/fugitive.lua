7110260c41f37654219adf12fbeafaa44457638e
Moshe Avni <moshe.avni@flexera.com>
2025-12-26 15:32:53 +0200

    fix fugitive ftplugin

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -1,4 +1,4 @@
 vim.schedule(function()
-  vim.keymap.set('n', 'gp', '<cmd>Git push<CR>', { desc = "Git push", silent = true, remap = false })
-  vim.keymap.set('n', 'gl', '<cmd>Git pull<CR>', { desc = "Git pull", silent = true, remap = false })
+  vim.keymap.set('n', 'gp', '<cmd>Git push<CR>', { desc = "Git push", silent = true, remap = false, buffer = 0 })
+  vim.keymap.set('n', 'gl', '<cmd>Git pull<CR>', { desc = "Git pull", silent = true, remap = false, buffer = 0 })
 end)
