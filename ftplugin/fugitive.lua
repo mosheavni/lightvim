f1fd3c5226a3fa19ea53e725b4e00a131568436f
Moshe Avni <moshe.avni@flexera.com>
2026-01-07 14:37:25 +0200

    fix keymap

--- ftplugin/fugitive.lua
+++ ftplugin/fugitive.lua
@@ -2,6 +2,6 @@ vim.schedule(function()
   vim.keymap.set('n', 'gp', '<cmd>execute "Git push -u origin " . FugitiveHead()<CR>',
     { desc = "Git push", silent = true, remap = false, buffer = 0 })
   vim.keymap.set('n', 'gl', '<cmd>Git pull<CR>', { desc = "Git pull", silent = true, remap = false, buffer = 0 })
-  vim.keymap.set('n', 'gl', '<cmd>Git fetch --all --tags<CR>',
+  vim.keymap.set('n', 'gf', '<cmd>Git fetch --all --tags<CR>',
     { desc = "Git fetch all remote and tags", silent = true, remap = false, buffer = 0 })
 end)
