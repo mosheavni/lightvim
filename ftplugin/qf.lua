32674a6f045ae6e00b6b355ddd21be9722161318
Moshe Avni <moshe.av@appcharge.com>
2026-06-17 13:58:15 +0300

    autocmds and quickfix

---
+++ ftplugin/qf.lua
@@ -0,0 +1,68 @@
+local open_quickfix = function(new_split_cmd)
+  local qf_idx = vim.fn.line '.'
+  vim.cmd 'wincmd p'
+  vim.cmd(new_split_cmd)
+  vim.cmd(qf_idx .. 'cc')
+end
+vim.keymap.set('n', '<c-v>', function()
+  open_quickfix 'vnew'
+end, { buffer = true })
+
+vim.keymap.set('n', '<C-s>', function()
+  open_quickfix 'split'
+end, { buffer = true })
+
+vim.keymap.set('n', '<C-t>', function()
+  open_quickfix 'tabnew'
+end, { buffer = true, desc = 'Open quickfix item in new tab' })
+
+local function remove_qf_items(start_line, end_line)
+  local qf_list = vim.fn.getqflist()
+  if #qf_list == 0 then
+    return
+  end
+
+  -- Remove items in reverse order to maintain indices
+  for i = end_line, start_line, -1 do
+    if i >= 1 and i <= #qf_list then
+      table.remove(qf_list, i)
+    end
+  end
+
+  vim.fn.setqflist(qf_list, 'r')
+
+  -- Position cursor at the start line or the last available item
+  if #qf_list > 0 then
+    local new_pos = math.min(start_line, #qf_list)
+    vim.cmd(new_pos .. 'cfirst')
+  end
+  vim.cmd 'copen'
+end
+
+-- Operator function for removing quickfix items
+_G.op = _G.op or {}
+function _G.op.qf_delete_operator(_)
+  local start_line = vim.fn.line "'["
+  local end_line = vim.fn.line "']"
+  remove_qf_items(start_line, end_line)
+end
+
+-- Set up the operator mapping
+vim.keymap.set('n', 'd', function()
+  vim.o.operatorfunc = 'v:lua.op.qf_delete_operator'
+  return 'g@'
+end, { expr = true, buffer = true, desc = 'Delete quickfix items' })
+
+-- Also support dd directly for line-wise delete
+vim.keymap.set('n', 'dd', function()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  remove_qf_items(line, line)
+end, { buffer = true, desc = 'Delete quickfix item' })
+
+-- map yy to yank file name
+vim.keymap.set('n', 'yy', function()
+  local line = vim.api.nvim_get_current_line()
+  local filename = vim.split(line, ' ')[1]
+  vim.fn.setreg('"', filename)
+  vim.notify('Copied ' .. filename .. ' to register')
+end, { remap = false, buffer = true })
--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -41,3 +41,80 @@ vim.api.nvim_create_autocmd('TextYankPost', { -- yank-ring
     end
   end,
 })
+
+-- Make file executable when a shebang is detected
+vim.api.nvim_create_autocmd('BufWritePost', {
+  group = group,
+  desc = 'Make sh file executable if a shebang is detected',
+  pattern = '*',
+  callback = function(args)
+    local shebang = vim.api.nvim_buf_get_lines(args.buf, 0, 1, true)[1]
+    if not shebang or not shebang:match '^#!.+' then
+      return
+    end
+    local filename = vim.api.nvim_buf_get_name(args.buf)
+    if filename == '' then
+      return
+    end
+    local fileinfo = vim.uv.fs_stat(filename)
+    if not fileinfo or vim.uv.fs_access(filename, 'X') then
+      return
+    end
+    -- selene: allow(undefined_variable)
+    if vim.uv.fs_chmod(filename, bit.bor(fileinfo.mode, 493)) then
+      vim.notify 'File made executable'
+    end
+  end,
+})
+
+-- Reload the file when it changed outside of Neovim
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = group,
+  desc = 'Check if the file changed when regaining focus',
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd 'checktime'
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('FileChangedShellPost', {
+  group = group,
+  desc = 'Reload when the file is changed outside of Neovim',
+  callback = function()
+    vim.notify('File changed, reloading the buffer', vim.log.levels.WARN)
+  end,
+})
+
+-- Spell checking for prose filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  group = group,
+  pattern = { 'markdown', 'gitcommit', 'text' },
+  desc = 'Enable spell checking for prose filetypes',
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { 'en' }
+    vim.api.nvim_set_hl(0, 'SpellBad', {
+      undercurl = true,
+      sp = '#ff5555',
+    })
+  end,
+})
+
+-- Quit with q in these filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  group = group,
+  desc = 'Quit with q in these filetypes',
+  pattern = {
+    'checkhealth',
+    'help',
+    'man',
+    'notify',
+    'qf',
+    'lspinfo',
+    'startuptime',
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
