e3d86bfdc2827179303b3bc1085556db063bd31b
Moshe Avni <moshe.av@appcharge.com>
2026-06-18 12:31:34 +0300

    remove color

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,13 +4,14 @@ vim.api.nvim_create_autocmd('PackChanged', {
   callback = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
     if name == 'nvim-treesitter' and (kind == 'install' or kind == 'update') then
-      if not ev.data.active then vim.cmd.packadd 'nvim-treesitter' end
+      if not ev.data.active then
+        vim.cmd.packadd 'nvim-treesitter'
+      end
       vim.cmd 'TSUpdate'
     end
   end,
 })
 
-require 'plugins.color'
 require 'plugins.mini'
 require 'plugins.lazydev'
 
